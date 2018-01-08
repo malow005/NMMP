8268a83b2f065a100893fc76c9d5893717d53836
HickeyLeong <qq1929754017@outlook.com>
2018-01-08 21:02:29 +0800

    Update header.php

--- header.php
+++ header.php
@@ -85,7 +85,7 @@
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
 			      </button>
-			      <a class="navbar-brand" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a>
+			      <a style="width:170px" class="navbar-brand" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a>
 			    </div>
 			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
 			      <ul class="nav navbar-nav">
